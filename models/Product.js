3423c2e52475f3ad2a4a34a2995f6d4a599f218a
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-05-11 15:59:31 -0300

    add product model

---
+++ models/Product.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    type: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = model('Product', productSchema);
+
+module.exports = Product;
