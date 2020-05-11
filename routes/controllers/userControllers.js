ef5b8f4542d958447aae00fdc773084a1a782942
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-05-11 19:32:49 -0300

    Create getUser and deleteUser controllers

--- routes/controllers/userControllers.js
+++ routes/controllers/userControllers.js
@@ -1,12 +1,31 @@
+const User = require('../../models/User');
+
 const userControllers = {
+  
   editUser() {
-    // TODO
+    const { id } = req.params;
+    const { name, number } = req.body;
+
+    User.findByIdAndUpdate(id, { name, number })
+      .then( user => res.json({user}))
+      .catch( error => res.status(404).json({ message: "user not found"}))
+
   },
+
   deleteUser() {
-    // TODO
+    const { id } = req.params;
+
+    User.findByIdAndDelete(id)
+      .then( user => res.json({ user }))
+      .error(error => res.status(500).json({ message: "user not found"}))
   },
-  getUsers() {
-    // TODO
+
+  getUser() {
+    const { id } = req.params;
+
+    User.findById(id)
+      .then( user => res.json({user}))
+      .catch( error => res.status(404).json({ message: "user not found"}))
   }
 }
 
