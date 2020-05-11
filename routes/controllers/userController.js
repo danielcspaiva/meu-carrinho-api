e6c62f9b51c1df4f54e7cf3932d410b4788bbe29
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-05-11 19:14:05 -0300

    Start composing User controllers

--- routes/controllers/userController.js
+++ routes/controllers/userController.js
@@ -1,5 +1,21 @@
-const userController = {
+const User = require('../../models/User');
 
+const userController = {
+  getUser(){
+    const { id } = req.params;
+
+    User.findById(id)
+      .then()
+      .catch()
+  },
+
+  editUser(){
+
+  },
+
+  deleteUser(){
+
+  }
 }
 
 module.exports = userController;
