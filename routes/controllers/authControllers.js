35239d07e4114f7e83e4b873bf64b162fdc53633
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-05-12 14:49:11 -0300

    initialize signup controller

--- routes/controllers/authControllers.js
+++ routes/controllers/authControllers.js
@@ -1,11 +1,30 @@
+const User = require('../../models/User');
+
 const authControllers = {
+  signup(req, res){
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      res.status(406).json({ message: 'name, email or password not provided' });
+      return;
+    }
+
+    if (password.length < 6) {
+      res.status(411).json({ messsage: 'password must have 6 characters at least' });
+      return;
+    }
+
+    User.find({ email })
+
+    User
+      .create()
+      .then((user) => res.status(201).json({ user }))
+      .catch((error) => res.status(500).json({ error }))
+  },
+
   login(){
     //to do
 
-  },
-
-  signup(){
-    //to do 
   },
 
   loggedin(){
