0d43179d592dd831ff0c1a55fca7fed1ecefd4b4
Daniel Paiva <danielcspaiva@gmail.com>
2020-05-11 21:16:41 -0300

    store controllers completed. Tests with user pending

--- routes/store.routes.js
+++ routes/store.routes.js
@@ -5,7 +5,7 @@ const storeControllers = require('./controllers/storeControllers.js');
 
 router.post('/new', storeControllers.createStore)
 router.patch('/edit/:id', storeControllers.editStore)
-router.delete('/delete/id', storeControllers.deleteStore)
+router.delete('/delete/:id', storeControllers.deleteStore)
 router.get('/:name', storeControllers.getStore)
 
 module.exports = router;
