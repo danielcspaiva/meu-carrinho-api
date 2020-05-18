cf37836d500e213f9bdbb0c9c06a45fd50180700
Daniel Paiva <danielcspaiva@gmail.com>
2020-05-18 16:49:38 -0300

    added facebook .env variables to fabook config and .env.example

--- configs/facebook.js
+++ configs/facebook.js
@@ -1,32 +1,33 @@
 var passport = require('passport')
 FacebookStrategy = require('passport-facebook').Strategy;
+require('dotenv').config()
 
 passport.use(new FacebookStrategy({
-    clientID: FACEBOOK_APP_ID,
-    clientSecret: FACEBOOK_APP_SECRET,
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
     callbackURL: "http://www.example.com/auth/facebook/callback"
   },
-  function(accessToken, refreshToken, profile, done) {
+  function (accessToken, refreshToken, profile, done) {
     User.findOne({
-      googleID: profile.id
-    })
-    .then(user => {
+        googleID: profile.id
+      })
+      .then(user => {
         if (user) {
-            done(null, user);
-            return;
+          done(null, user);
+          return;
         }
 
         User.create({
-                facebookID: profile.id,
-                username: profile.name.givenName,
-                email: profile.emails[0].value,
-                imageUrl: profile.photos[0].value
-            })
-            .then(newUser => {
-              console.log(user)
-            })
-            .catch(err => done(err)); // closes User.create()
-    })
-    .catch(err => done(err));
+            facebookID: profile.id,
+            username: profile.name.givenName,
+            email: profile.emails[0].value,
+            imageUrl: profile.photos[0].value
+          })
+          .then(newUser => {
+            console.log(user)
+          })
+          .catch(err => done(err)); // closes User.create()
+      })
+      .catch(err => done(err));
   }
 ));
