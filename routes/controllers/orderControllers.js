388c130fa73481145c683de679e12b3cf0713034
Daniel Paiva <danielcspaiva@gmail.com>
2020-05-19 18:15:41 -0300

    added email notification to store owner when new order is created

--- routes/controllers/orderControllers.js
+++ routes/controllers/orderControllers.js
@@ -1,6 +1,9 @@
 const Order = require('../../models/Order');
 const Store = require('../../models/Store');
 const Product = require('../../models/Product');
+const User = require('../../models/User')
+const transporter = require('../../configs/nodemailer');
+
 
 const orderControllers = {
   getOrder(req, res) {
@@ -30,8 +33,30 @@ const orderControllers = {
         promises.push(Store.findByIdAndUpdate(storeId, { $push: { orders: order }}));
         
         Promise.all(promises)
-          .then(() => res.status(200).json({ message: 'order created', order }))
-          .catch((error) => res.status(500).json({ error }));
+        .then(() => res.status(200).json({ message: 'order created', order }))
+        .catch((error) => res.status(500).json({ error }));
+
+        User.find({stores: storeId})
+          .then(user => {
+            transporter.sendMail({
+              from: '"Sacada do Prédio" <sacadadopredio@gmail.com>',
+              to: user[0].email,
+              subject: 'Novo ordem registrada',
+              text: ``,
+              html: `
+                  <p>Olá ${user[0].name},</p>
+
+                  <p>Uma nova ordem foi registrada na sua loja, confira acessando o dashboard da sua loja.</p>
+                  
+                  <p>Detalhes do pedido:${order}</p>
+
+                  <p>Muito obrigado,</p>
+
+                  <strong style="rgb(198, 72, 12)">Meu carrinho team</strong>
+              `
+            })
+          })
+          .catch(err => console.log(err))
       })
       .catch((error) => res.status(500).json({ error }));
   },
